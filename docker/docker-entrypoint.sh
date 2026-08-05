c0de55b4d4aab753d8f0d8cbf08796c6745e81e6
AlaaEid-1 <alaaalaa2382924@gmail.com>
2026-08-05 13:49:01 +0300

    update file entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,16 +5,12 @@ echo "Starting Laravel deployment..."
 
 php artisan storage:link || true
 
-# Create database
 php artisan migrate:fresh --force
 
-# Create admin user
 php artisan db:seed --force || true
 
-# Clear cache
-php artisan optimize:clear
+php artisan optimize:clear || true
 
-# Cache config only
 php artisan config:cache
 
 echo "Laravel deployment completed successfully."
